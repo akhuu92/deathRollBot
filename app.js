d3c72e0b68c7f58f77e99b6baf3f91ea9808241d
Israel Torres <itorres1490@gmail.com>
2019-10-25 06:25:35 -0700

    Separate most business logic from main client - Use replyWithHTML for better styling - Ran npm init to save modules in packages.json along with project info - Included lodash - And attempt to use async/await

--- app.js
+++ app.js
@@ -1,66 +1,33 @@
 require('dotenv').config()
 
 const Telegraf = require('telegraf')
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const MIN = 1;
-const MAX = 100;
+const session = require('telegraf/session')
+const utils = require('./src/util.js')
 
+const BOT_TOKEN = process.env.BOT_TOKEN
 
 if (!BOT_TOKEN) {
-  throw "Missing Bot API Key: (BOT_TOKEN). Please set it in ENV";
+  throw new Error('Missing Bot API Key: (BOT_TOKEN). Please set it in ENV')
 }
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-//ctx is the context for one Telegram update, next is a function that is invoked to execute the downstream middleware.
+bot.use(session())
 
-let state = {
-  playing : false,
-  lastRoll: MAX
-};
-
-bot.command('roll', (ctx) => {
-  var params = ctx.message.text.split(" ");
-
-  if (params.length > 1) {
-    if (params[1] === "new") {
-      if (params[2]) {
-        var newValue = Number.parseInt(params[2])
-        if (Number.isNaN(newValue) || isFinite(newValue) || newValue > Number.MAX_VALUE || newValue < 0) {
-          ctx.reply(`Please roll a valid positive number, defaulting to ${MAX}.`)
-          newValue = MAX;
-        }
-
-        state.lastRoll = newValue;
-      } else {
-        state.lastRoll = MAX;
-      }
-
-      state.playing = true;
-      ctx.reply(`${ctx.from.username} started a new Death Roll`);
-    }
+bot.command(['dr', 'deathroll'], (ctx) => {
+  const params = utils.parseParams(ctx.message.text)
+  const chatId = ctx.message.chat.id
+  const player = {
+    'playerId': ctx.message.from.id,
+    'displayName': utils.getDisplayName(ctx.message.from)
   }
 
-  if (!state.playing) {
-    ctx.reply("Please use /roll new [num] for a new game");
+  utils.startGame(ctx, params, chatId, player)
+
+  if (ctx.session.game.isPlaying) {
+    utils.throwDice(ctx, player)
   } else {
-    let lastRoll = state.lastRoll
-    var currentRoll = getRandomIntInclusive(MIN, lastRoll)
-  
-    if (currentRoll === 1) {
-      state.playing = false;
-      state.lastRoll = MAX;
-      ctx.reply(`${ctx.from.username} rolled: ${currentRoll}. You lost.\n/roll new [num] for a new game.`);
-    } else {
-      state.lastRoll = currentRoll;
-      ctx.reply(`${ctx.from.username} rolled: ${currentRoll}`);
-    }
+    ctx.replyWithHTML(`Use <code>/dr new [num]</code> for new ☠🎲`)
   }
 })
 
 bot.launch()
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-}
