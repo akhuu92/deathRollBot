055386d7452cae580209b18c81168eeece248e2c
Alvin <akhuu92@gmail.com>
2019-10-31 18:16:57 -0700

    Allow multiple games to run on different chatrooms

--- app.js
+++ app.js
@@ -12,15 +12,12 @@ if (!BOT_TOKEN) {
 const bot = new Telegraf(process.env.BOT_TOKEN)
 // ctx is the context for one Telegram update, next is a function that is invoked to execute the downstream middleware.
 
-const state = {
-  playing: false,
-  lastRoll: MAX
-}
+const state = {}
 
 bot.command('roll', (ctx) => {
   var params = ctx.message.text.split(' ')
+  console.log(ctx.from)
   startGame(ctx, params)
-  
 })
 
 bot.command('help', (ctx) => {
@@ -35,39 +32,54 @@ function getRandomIntInclusive (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min // The maximum is inclusive and the minimum is inclusive
 }
 
+function getName (from) {
+  if (from.username) {
+    return from.username
+  } else {
+    return from.first_name + ' ' + from.last_name
+  }
+}
+
 async function startGame (ctx, params) {
+  var name = getName(ctx.from)
+  var chatId = ctx.message.chat.id
+
   if (params.length > 1) {
     if (params[1] === 'new') {
+      state[chatId] = {
+        playing: false,
+        lastRoll: MAX
+      }
+
       if (params[2]) {
         var newValue = Number.parseInt(params[2])
         if (Number.isNaN(newValue) || newValue > Number.MAX_VALUE || newValue < 0) {
           ctx.reply(`Please roll a valid positive number, defaulting to ${MAX}.`)
           newValue = MAX
         }
-
-        state.lastRoll = newValue
+        state[chatId].lastRoll = newValue
       } else {
-        state.lastRoll = MAX
+        state[chatId].lastRoll = MAX
       }
 
-      state.playing = true
-    await ctx.reply(`${ctx.from.username} started a new Death Roll.`)
+      state[chatId].playing = true
+      await ctx.reply(`${name} started a new Death Roll.`)
     }
   }
 
-  if (!state.playing) {
+  if (!state[chatId].playing) {
     ctx.reply('Please use /roll new [num] for a new game')
   } else {
-    const lastRoll = state.lastRoll
+    const lastRoll = state[chatId].lastRoll
     var currentRoll = getRandomIntInclusive(MIN, lastRoll)
 
     if (currentRoll === 1) {
-      state.playing = false
-      state.lastRoll = MAX
-      ctx.reply(`${ctx.from.username} rolled: ${currentRoll}. You lost.\n/roll new [num] for a new game.`)
+      state[chatId].playing = false
+      state[chatId].lastRoll = MAX
+      ctx.reply(`${name} rolled: ${currentRoll}. You lost.\n/roll new [num] for a new game.`)
     } else {
-      state.lastRoll = currentRoll
-      ctx.reply(`${ctx.from.username} rolled: ${currentRoll}`)
+      state[chatId].lastRoll = currentRoll
+      ctx.reply(`${name} rolled: ${currentRoll}`)
     }
   }
 }
